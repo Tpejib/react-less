4f6735c6047f6859fb5536ae566acda65c5306dd
Trel1 <trel@netcraft.global>
2020-07-19 19:23:46 +0300

    less 1.9 posts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,14 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'asdasd'},
+        {label: 'That is so good', important: false, id: 'sqweqw'},
+        {label: 'I need a break...', important: false, id: 'zxczxc'},
+        123123, '12312', {}
+    ];
+
     return (
         <div className='app'>
             <AppHeader/>
@@ -15,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data} />
             <PostAddForm/>
         </div>
     )
